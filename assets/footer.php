ed926cc093273b4972b478ff02164328e2c2b7a8
Vinicius Santos <vinicius.m.dos.s@gmail.com>
2024-08-09 02:49:47 -0300

    :lipstick: Improving header and footer

--- assets/footer.php
+++ assets/footer.php
@@ -1,2 +1,7 @@
+    <footer class="footer text-center">
+        <div class="container">
+            <p>&copy; 2024 Gerenciador de OS. Todos os direitos reservados.</p>
+        </div>
+    </footer>
 </body>
 </html>
